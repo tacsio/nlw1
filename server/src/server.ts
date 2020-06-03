db8cf07f4a101d068bcc977238aea0f21de1da39
Tarcisio Coutinho <tcs5cin@gmail.com>
2020-06-03 12:43:40 -0300

    server implementation

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import routes from './routes';
+import path from 'path';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-	response.json(
-		['User1', 'User2']
-	)
-});
+app.use(cors())
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333);
